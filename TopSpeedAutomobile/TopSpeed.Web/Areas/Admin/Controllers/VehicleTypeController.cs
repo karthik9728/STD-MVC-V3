39dc1f69fbb2cf6ebe2fb06a1d9ab2c3308ce790
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-06-08 03:18:30 +0530

    VehicleType_Index_Details

--- TopSpeedAutomobile/TopSpeed.Web/Areas/Admin/Controllers/VehicleTypeController.cs
+++ TopSpeedAutomobile/TopSpeed.Web/Areas/Admin/Controllers/VehicleTypeController.cs
@@ -1,13 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TopSpeed.DataAccess.Common;
+using TopSpeed.Domain.Models;
 
 namespace TopSpeed.Web.Areas.Admin.Controllers
 {
     public class VehicleTypeController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
 
-        public IActionResult Index()
+        public VehicleTypeController(ApplicationDbContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<VehicleType> vehicleTypes = await _dbContext.VehicleType.ToListAsync();
+
+            return View(vehicleTypes);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            VehicleType vehicleType = await _dbContext.VehicleType.FirstOrDefaultAsync(x=>x.Id == id);
+
+            return View(vehicleType);
         }
     }
 }
