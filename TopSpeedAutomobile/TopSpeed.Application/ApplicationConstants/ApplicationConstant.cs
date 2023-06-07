e69f4ebf27bd4daef048f209fbf97af36963fa1a
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-06-08 00:53:50 +0530

    ApplicationFiles

---
+++ TopSpeedAutomobile/TopSpeed.Application/ApplicationConstants/ApplicationConstant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSpeed.Application.ApplicationConstants
+{
+    public class ApplicationConstant
+    {
+    }
+}
